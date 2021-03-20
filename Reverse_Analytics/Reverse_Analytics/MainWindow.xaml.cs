eb867970df44e947bc1eccb37d890cc305f81400
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2021-03-20 14:36:52 +0100

    initial commit

---
+++ Reverse_Analytics/Reverse_Analytics/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using Infrastructure.Models;
+
+namespace Reverse_Analytics
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Client c = new Client();
+
+            System.Console.WriteLine(c);
+        }
+    }
+}
