11125b1a995ead0ab56970f5a8bb3f0315c67ec8
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2021-03-21 16:41:10 +0100

    add navigation vw implementation

--- Reverse_Analytics/Reverse.Modules.ModuleNames/ViewModels/NavigationViewModel.cs
+++ Reverse_Analytics/Reverse.Modules.ModuleNames/ViewModels/NavigationViewModel.cs
@@ -1,10 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Prism.Commands;
+using Prism.Regions;
+using Reverse_Analytics.Core;
+using Reverse_Analytics.Core.MVVM;
+using Reverse_Analytics.Core.Resources;
+using System.Linq;
 
 namespace Reverse.Modules.ModuleNames.ViewModels
 {
-    class NavigationViewModel
+    public class NavigationViewModel : RegionViewModelBase
     {
+        #region Commands
+
+        public DelegateCommand<string> NavigateCommand { get; set; }
+        public DelegateCommand LogoutCommand { get; set; }
+
+        #endregion
+
+        #region Form properties
+
+        private string _navigationText;
+        public string NavigationText 
+        {
+            get => _navigationText;
+            set => SetProperty(ref _navigationText, value);
+        }
+
+        private string _login;
+        private string Login
+        { 
+            get => _login;
+            set => SetProperty(ref _login, value);    
+        }
+
+        #endregion
+
+        public NavigationViewModel(IRegionManager regionManager) : base(regionManager)
+        {
+            NavigationText = "Navigation...";
+            NavigateCommand = new DelegateCommand<string>(Navigate);
+            LogoutCommand = new DelegateCommand(Logout);
+        }
+
+        public override void OnNavigatedFrom(NavigationContext navigationContext)
+        {
+            if (navigationContext.Parameters.ContainsKey(nameof(Login)))
+                Login = $"{Strings.User}: {navigationContext.Parameters.GetValue<string>(nameof(Login))}";
+        }
+
+        private void Logout()
+        {
+            ClearAllRegions();
+
+            RegionManager.RequestNavigate(RegionNames.ContentRegion, ViewNames.LoginView);
+        }
+
+        private void ClearAllRegions() =>
+            RegionManager.Regions.ToList().ForEach(region => region.RemoveAll());
+
+        private void Navigate(string uri) => 
+            RegionManager.RequestNavigate(RegionNames.ContentRegion, uri);
     }
 }
--- Reverse_Analytics/Reverse_Analytics.Core/Resources/Strings.Designer.cs
+++ Reverse_Analytics/Reverse_Analytics.Core/Resources/Strings.Designer.cs
@@ -22,7 +22,7 @@ namespace Reverse_Analytics.Core.Resources {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Strings {
+    public class Strings {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace Reverse_Analytics.Core.Resources {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Reverse_Analytics.Core.Resources.Strings", typeof(Strings).Assembly);
@@ -51,12 +51,21 @@ namespace Reverse_Analytics.Core.Resources {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Пользователь.
+        /// </summary>
+        public static string User {
+            get {
+                return ResourceManager.GetString("User", resourceCulture);
             }
         }
     }
