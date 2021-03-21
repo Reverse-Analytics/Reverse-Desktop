f9f2ba31b96248a5a74f68ec6af8a42fd89c4342
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2021-03-21 20:16:07 +0100

    add clients & suppliers initial view, vm

---
+++ Reverse_Analytics/Reverse.Modules.Forms/ViewModels/ClientsViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Prism.Regions;
+using Reverse_Analytics.Core.MVVM;
+using System;
+
+namespace Reverse.Modules.Forms.ViewModels
+{
+    class ClientsViewModel : RegionViewModelBase
+    {
+        private IRegionNavigationJournal _journal;
+
+        public ClientsViewModel(IRegionManager regionManager) : base(regionManager)
+        {
+        }
+
+        private bool CanExecute()
+        {
+            return true;
+        }
+
+        private void Cancel()
+        {
+            _journal.GoBack();
+        }
+
+        private void Execute()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnNavigatedTo(NavigationContext navigationContext)
+        {
+            base.OnNavigatedTo(navigationContext);
+
+            _journal = navigationContext.NavigationService.Journal;
+        }
+    }
+}
---
+++ Reverse_Analytics/Reverse.Modules.Forms/ViewModels/SuppliersViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Prism.Regions;
+using Reverse_Analytics.Core.MVVM;
+using System;
+
+namespace Reverse.Modules.Forms.ViewModels
+{
+    public class SuppliersViewModel : RegionViewModelBase
+    {
+        IRegionNavigationJournal _journal;
+
+        public SuppliersViewModel(IRegionManager regionManager) : base(regionManager)
+        {
+        }
+
+        private bool CanExecute()
+        {
+            return true;
+        }
+
+        private void Cancel()
+        {
+            _journal.GoBack();
+        }
+
+        private void Execute()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnNavigatedTo(NavigationContext navigationContext)
+        {
+            base.OnNavigatedTo(navigationContext);
+
+            _journal = navigationContext.NavigationService.Journal;
+        }
+    }
+}
