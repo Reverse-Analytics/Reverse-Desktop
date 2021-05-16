e0394d4c4ea8fd13a6c27ff00ed5372c8cd845ac
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2021-05-16 22:11:38 +0200

    add clock indicator

---
+++ Reverse_Analytics/Infrastructure/Reverse.Infrastructure.CustomControls/Common/Clock.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Reverse.Infrastructure.CustomControls.Common
+{
+    public class Clock : DependencyObject
+    {
+        public Clock()
+        {
+            StartTimer();
+        }
+
+        void StartTimer()
+        {
+            DispatcherTimer timer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(100) };
+            timer.Tick += (s, e) => { DateTime = DateTime.Now; };
+            timer.Start();
+        }
+
+        public DateTime DateTime
+        {
+            get { return (DateTime)GetValue(DateTimeProperty); }
+            set { SetValue(DateTimeProperty, value); }
+        }
+
+        public static readonly DependencyProperty DateTimeProperty =
+            DependencyProperty.Register(nameof(DateTime), typeof(DateTime), typeof(Clock));
+    }
+}
