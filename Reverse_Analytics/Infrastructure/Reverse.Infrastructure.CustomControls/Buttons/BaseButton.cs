fae9df382508676d03e6fbfcd63cf15126db2e87
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2021-05-19 18:15:58 +0200

    add custom navigation button

---
+++ Reverse_Analytics/Infrastructure/Reverse.Infrastructure.CustomControls/Buttons/BaseButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+namespace Reverse.Infrastructure.CustomControls.Buttons
+{
+    public class BaseButton : Button
+    {
+        public string ButtonName { get; set; }
+        
+        public BaseButton()
+        {
+        }
+    }
+}
---
+++ Reverse_Analytics/Infrastructure/Reverse.Infrastructure.CustomControls/Buttons/NavigationButton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Reverse.Infrastructure.CustomControls.Buttons
+{
+    /*
+     * Sidebar navigation button
+     */
+    public class NavigationButton : BaseButton
+    {
+        public NavigationButton()
+            : base()
+        {
+            Width = 190;
+            BorderThickness = new Thickness(0,0,0,0);
+            FontSize = 18;
+            Foreground = new SolidColorBrush(Colors.White);
+            Background = (SolidColorBrush)(new BrushConverter().ConvertFrom("#363740"));
+        }
+    }
+}
