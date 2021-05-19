6c385158130bb3359820417d4a46978bc5c68624
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2021-05-19 20:23:05 +0200

    add AddButton custom control

--- Reverse_Analytics/Infrastructure/Reverse.Infrastructure.CustomControls/Buttons/AddButton.cs
+++ Reverse_Analytics/Infrastructure/Reverse.Infrastructure.CustomControls/Buttons/AddButton.cs
@@ -1,11 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Windows.Media;
 
 namespace Reverse.Infrastructure.CustomControls.Buttons
 {
     public class AddButton : BaseButton
     {
-
+        public AddButton()
+        {
+            Margin = new System.Windows.Thickness(25, 15, 5, 5);
+            HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
+            Width = 120;
+            Height = 35;
+            FontSize = 18;
+            Background = (SolidColorBrush)(new BrushConverter().ConvertFrom("#5CB85C"));
+            Foreground = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FFFFFF"));
+        }
     }
 }
