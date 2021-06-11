40120df2d99b728cf4a2e8e61b35ab706e86e287
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2021-06-11 16:23:16 +0200

    add tab person information control & tab navigation

---
+++ Reverse_Analytics/Modules/Reverse.Modules.Forms/ViewModels/PersonInformationViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Prism.Regions;
+using Reverse_Analytics.Core.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reverse.Modules.Forms.ViewModels
+{
+    public class PersonInformationViewModel : RegionViewModelBase
+    {
+
+        #region Properties
+
+        private string _title;
+        public string Title 
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        #endregion
+
+        public PersonInformationViewModel(IRegionManager regionManager) : base(regionManager)
+        {
+
+        }
+    }
+}
---
+++ Reverse_Analytics/Modules/Reverse.Modules.Forms/Views/PersonInformation.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Reverse.Modules.Forms.Views
+{
+    /// <summary>
+    /// Interaction logic for PersonInformation.xaml
+    /// </summary>
+    public partial class PersonInformation : UserControl
+    {
+        public PersonInformation()
+        {
+            InitializeComponent();
+        }
+    }
+}
