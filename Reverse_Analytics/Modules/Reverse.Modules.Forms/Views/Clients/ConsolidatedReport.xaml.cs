b7c1fbbb19e2f9cf574601c95b05abb203f53e2b
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2021-07-07 22:22:49 +0200

    add consolidated report control & move client information to separate control

---
+++ Reverse_Analytics/Modules/Reverse.Modules.Forms/Views/Clients/ConsolidatedReport.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Reverse.Modules.Forms.Views.Clients
+{
+    /// <summary>
+    /// Interaction logic for ConsolidatedReport.xaml
+    /// </summary>
+    public partial class ConsolidatedReport : UserControl
+    {
+        public ConsolidatedReport()
+        {
+            InitializeComponent();
+        }
+    }
+}
